45348271492b081e14ab2dda6f3139e028bbbf21
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-05-11 05:41:38 -0500

    added about and skills section

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <div id="section1">
       <Main />
+      </div>
       <div id="section2">
         <About />
       </div>
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import '../Styling/main.css';
+import { skills } from '../data.js';
 
 function About() {
   return (
     <div className='about-container'>
       <h1>ABOUT</h1>
-      <p>
-        Hello! I'm Erik Correa, a NYC based software engineer.(willing to relocate) I am experienced in full stack development. 
-        My skills allow me to tackle complex programming problems presented on the job. 
-        My love for programming stems from my passion to know how things work, how to build them, and how to fix them. 
-        Contact me to get started working together!
-      </p>
+      <div className='about-para'>
+        <p>
+          Hello! I'm Erik Correa, a NYC based software engineer.(willing to relocate) I am experienced in full stack development. 
+          My skills allow me to tackle complex programming problems presented on the job. 
+          My love for programming stems from my passion to know how things work, how to build them, and how to fix them. 
+          Contact me to get started working together!
+        </p>
+      </div>
+
+      <h1 className='about-skills'>SKILLS</h1>
+      <div className="skills-container">
+            {skills.map((skill) => (
+                <div key={skill} className="card">
+                    <div className="card-body">
+                        <span className="card-text">{skill}</span>
+                    </div>
+                </div>
+            ))}
+      </div>
     </div>
   )
 }
