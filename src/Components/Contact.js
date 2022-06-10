417b6629a68eb735224629c67173f9a3ab221a8a
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-06-09 21:51:57 -0500

    added resume button

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,7 +6,10 @@ function Contact() {
     <>
       <h1 className='contact-h1'>CONTACT</h1>
       <div className='button-container'>
-        <button className='email-button'><a className="email" href = "mailto: erikcorrea444@gmail.com">email me</a></button>
+        <button className='email-button'><a className="email" href = "mailto: erikcorrea444@gmail.com">EMAIL ME</a></button>
+      </div>
+      <div className='button-container'>
+        <button className='email-button'><a className="email" href ="https://docs.google.com/document/d/1V_qwBwpUjXztFb_fDUlu3GlAA5JotlzC9aCKgcIGqzU/edit?usp=sharing">RESUME</a></button>
       </div>
     </>
   )
