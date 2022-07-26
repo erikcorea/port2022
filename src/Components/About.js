3b73771c56110e0a125c4af7bad087ecf42a7363
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-07-26 12:57:26 -0500

    changed about

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ function About() {
       <h1>ABOUT</h1>
       <div className='about-para'>
         <p>
-          Hello! I'm Erik Correa, a NYC based software engineer.(willing to relocate) I am experienced in full stack development. 
+          Hello! I'm Erik Correa, a software engineer.(willing to relocate) I am experienced in full stack development. 
           My skills allow me to tackle complex programming problems presented on the job. 
           My love for programming stems from my passion to know how things work, how to build them, and how to fix them. 
           Contact me to get started working together!
